a1c4c5be2cc0d8244b5c2330f3edee6bd5d3f318
Yasuaki Uechi <y@uechi.io>
2019-05-24 20:06:18 +0900

    prettier

--- examples/worker.js
+++ examples/worker.js
@@ -1,4 +1,4 @@
-importScripts('https://cdn.jsdelivr.net/npm/comlinkjs@3.0.2/umd/comlink.js');
-importScripts('../ffmpeg-webm.js');
+importScripts('https://cdn.jsdelivr.net/npm/comlinkjs@3.0.2/umd/comlink.js')
+importScripts('../ffmpeg-webm.js')
 
-Comlink.expose(ffmpegjs, self);
+Comlink.expose(ffmpegjs, self)
